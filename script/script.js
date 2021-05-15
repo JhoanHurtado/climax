5cb9422571fa1b4d4e279e105fb2425beb97b935
JhoanHurtado <36869403+JhoanHurtado@users.noreply.github.com>
2021-05-14 21:16:31 -0500

    seconds
    as

--- script/script.js
+++ script/script.js
@@ -2,15 +2,19 @@ var city = 'bogota';
 var API_KEY = "a891e645e9e2390ccf7a1d377acc12f7";
 var current_main = document.getElementById('curret-main');
 var curret_grades = document.getElementById('curret_grades');
+var current_time_icon = document.getElementById('current_time_icon');
 
 var Fmain = document.getElementById('Pmain');
 var Fgrades = document.getElementById('Pgrades');
 var Pdescription = document.getElementsByClassName('Pdescription');
+var paris_time_icon = document.getElementById('paris_time_icon');
 
 
 var Lmain = document.getElementById('Lmain');
 var Lgrades = document.getElementById('Lgrades');
 var Ldescription = document.getElementsByClassName('Ldescription');
+var lyon_time_icon = document.getElementById('lyon_time_icon');
+
 
 var r = new XMLHttpRequest();
 
@@ -19,9 +23,11 @@ r.onreadystatechange = function () {
     if (r.readyState != 4 || r.status != 200) return;
     var clima = JSON.parse(r.responseText);
     current_main.innerHTML = clima['weather'][0].main;
-    curret_grades.innerHTML = Math.round(clima['main'].temp)+"°C";
+    curret_grades.innerHTML = Math.round(clima['main'].temp) + "°C";
+    current_time_icon.setAttribute("src", "http://openweathermap.org/img/wn/" + clima['weather'][0].icon + ".png");
 
 };
+r.send();
 
 r.open("GET", "http://api.openweathermap.org/data/2.5/weather?units=Celsius&q=Francia&appid=" + API_KEY, true);
 r.onreadystatechange = function () {
@@ -30,9 +36,10 @@ r.onreadystatechange = function () {
     Fmain.innerHTML = clima['weather'][0].main;
     Pdescription[0].innerHTML = clima['weather'][0].description;
     Pdescription[1].innerHTML = clima['weather'][0].description;
-    Fgrades.innerHTML = Math.round(clima['main'].temp)+"°C";
-
+    Fgrades.innerHTML = Math.round(clima['main'].temp) + "°C";
+    paris_time_icon.setAttribute("src", "http://openweathermap.org/img/wn/" + clima['weather'][0].icon + ".png");
 };
+r.send();
 
 r.open("GET", "http://api.openweathermap.org/data/2.5/weather?units=Celsius&q=Lyon&appid=" + API_KEY, true);
 r.onreadystatechange = function () {
@@ -41,9 +48,9 @@ r.onreadystatechange = function () {
     Lmain.innerHTML = clima['weather'][0].main;
     Ldescription[0].innerHTML = clima['weather'][0].description;
     Ldescription[1].innerHTML = clima['weather'][0].description;
-    Lgrades.innerHTML = Math.round(clima['main'].temp)+"°C";
-
+    Lgrades.innerHTML = Math.round(clima['main'].temp) + "°C";
+    lyon_time_icon.setAttribute("src", "http://openweathermap.org/img/wn/" + clima['weather'][0].icon + ".png");
 };
 
 
-r.send("banana=yellow");
+r.send();
